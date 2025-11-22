2d0c3b05c82222ca2c928a6ad0505884c35be382
mmancino1982-wq <mmancino1982@gmail.com>
2025-11-21 18:45:08 -0600

    Updates

--- michael-mancino-author-site/js/script.js
+++ michael-mancino-author-site/js/script.js
@@ -33,34 +33,3 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         }
     });
 });
-```
-
----
-
-## **NOW - THE IMAGES:**
-
-You need to create a folder called `images` and put two files in it:
-
-1. **Broken_Creed_Cover_HighRes.png** - Your book cover (you already have this)
-2. **author-photo.png** - Your headshot (you already have this)
-
-Just rename them to match exactly what I put above.
-
----
-
-## **FOLDER STRUCTURE:**
-
-Your final folder should look like this:
-```
-michael-mancino-author-site/
-├── index.html
-├── about.html
-├── book.html
-├── contact.html
-├── css/
-│   └── style.css
-├── js/
-│   └── script.js
-└── images/
-    ├── Broken_Creed_Cover_HighRes.png
-    └── author-photo.png
